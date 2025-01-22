c17fda2872025f50d46a09b1df296d419a54c402
Sadaf-A <sadaf20043006@gmail.com>
2025-01-22 22:04:08 +0530

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import RegisterForm from "./components/Auth/RegisterForm";
 import LoginForm from "./components/Auth/LoginForm";
 import {GoQuantDashboard} from "./components/Dashboard/Dashboard";
 import TradeComponent from "./components/Trade/TradeComponent";
@@ -9,11 +8,10 @@ const App = () => {
   return (
     <Router>
         <Routes>
-          <Route path="/register" element={<RegisterForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/dashboard" element={<GoQuantDashboard />} />
           <Route path="/trade" element={<TradeComponent />} />
-          <Route path="/" element={<RegisterForm />}/>
+          <Route path="/" element={<LoginForm />}/>
         </Routes>
     </Router>
   );
